fcd789449b049fc54b01fa075919555b1629fd75
Yudhishthir <yudhishthir.it21@gmail.com>
2021-05-14 20:49:03 +0530

    [Add Basic Layout]:  -Add header and footer partials  -Add shim and defaults partials  -Configure app for bootstrap 5 and jquery  -Add site layout integration test

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import '../js/bootstrap_js_files.js'
+require("jquery")
 
 Rails.start()
 Turbolinks.start()
