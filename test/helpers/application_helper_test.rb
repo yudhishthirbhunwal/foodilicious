59fdad70b12e840812c0daa65823da98e1048256
Yudhishthir <yudhishthir.it21@gmail.com>
2021-05-14 17:06:02 +0530

    [Add Static Pages]:  -Add static pages controller with required views  -Add test db files to gitignore file  -Add full title helper for application  -Add tests for static pages controller  -Add test for full title helper in application helper test  -Add required routes  -Include application helper in test helper

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+
+  test "full title helper" do
+    assert_equal full_title, "Foodilicious"
+    assert_equal full_title("Help"), "Help | Foodilicious"
+  end
+
+end
