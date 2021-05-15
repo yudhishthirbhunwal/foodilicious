e8f39cd83d78ecfba8a8c0a2d362f9775554e521
Yudhishthir <yudhishthir.ongraph@gmail.com>
2021-05-15 19:50:42 +0530

    [Implement Devise]:  -Add authentication with devise  -Add additional user attributes to devise migration  -Add flash messages partial  -Edit devise view to accept the additional attributes  -Permit the additional attributes in the devise params sanitizer  -Authenticate before every action except for static pages  -Configure action mailer for development and production env.  -Include devise helpers in test helper

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,19 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  host = 'foodilicious-app.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    authentication: :plain,
+    user_name: Rails.application.credentials.gmail[:GMAIL_USERNAME],
+    password: Rails.application.credentials.gmail[:GMAIL_PASSWORD],
+    domain: 'gmail.com',
+    enable_starttls_auto: true
+    }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -117,4 +129,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # Handling of unpermitted keys.
+  config.action_controller.action_on_unpermitted_parameters = :log
 end
