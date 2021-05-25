c2f327272e207c709e9ef3a96c6a019bf6ddaeab
Yudhishthir <yudhishthir.ongraph@gmail.com>
2021-05-25 14:43:43 +0530

    [Minor Fixes]:  -Fix users delete link  -Add seller info on foods index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @orders = @user.get_all_orders_for(current_user).page(params[:page]).per(10) if @user.is_buyer?
   end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      flash[:alert] = "User deleted successfully!"
+    else
+      flash[:alert] = "Error deleting the user!"
+    end
+    redirect_to users_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'help', to: 'static_pages#help'
   get 'about', to: 'static_pages#about'
   get 'contact', to: 'static_pages#contact'
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :destroy]
   resources :food_items
   resources :orders, only: [:index, :create, :destroy]
 end
