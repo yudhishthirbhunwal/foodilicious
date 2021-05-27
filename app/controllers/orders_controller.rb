67e969d68614b16d6d2b51698314ebaabb76d847
Yudhishthir <yudhishthir.ongraph@gmail.com>
2021-05-27 17:03:41 +0530

    [Minor Fix]:  -Minor fix in orders controller  -CSS selector fix for flash messages

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,30 +10,29 @@ class OrdersController < ApplicationController
     @food_item = FoodItem.find(params[:food_item_id])
     @order = current_user.orders.build(order_params)
     @order.food_item_id = @food_item.id
-    @order.save
+    if @order.save
+      UserMailer.with(food_item: @food_item, buyer: current_user, seller: @food_item.user).seller_order_email.deliver_now
+      UserMailer.with(food_item: @food_item, buyer: current_user, seller: @food_item.user).buyer_order_email.deliver_now
+    end
     @food_item.increment!(:order_count)
     respond_to do |format|
       format.html { redirect_to @food_item, notice: "AJAX Order placed Successfully!" }
       format.js
     end
     # flash[:notice] = "Order placed successfully!"
-    # UserMailer.with(food_item: @food_item, buyer: current_user, seller: @food_item.user).seller_order_email.deliver_now
-    # UserMailer.with(food_item: @food_item, buyer: current_user, seller: @food_item.user).buyer_order_email.deliver_now
-    # redirect_back(fallback_location: root_path)
   end
   
   def destroy
     @order = Order.find(params[:id])
     @food_item = @order.food_item
-    # UserMailer.with(food_item: @order.food_item, buyer: current_user, seller: @order.food_item.user).seller_cancel_email.deliver_now
-    # UserMailer.with(food_item: @order.food_item, buyer: current_user, seller: @order.food_item.user).buyer_cancel_email.deliver_now
+    UserMailer.with(food_item: @order.food_item, buyer: current_user, seller: @order.food_item.user).seller_cancel_email.deliver_now
+    UserMailer.with(food_item: @order.food_item, buyer: current_user, seller: @order.food_item.user).buyer_cancel_email.deliver_now
     @order.delete
     respond_to do |format|
       format.html { redirect_to food_items_path, alert: "AJAX Order Cancelled!" }
       format.js
     end
     # flash[:alert] = "Order Cancelled!"
-    # redirect_back(fallback_location: root_path)
   end
 
   private
