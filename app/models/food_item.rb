7e6f6ba3466b10e7c6dd12e679d322b3f93e2df9
Yudhishthir <yudhishthir.ongraph@gmail.com>
2021-05-27 10:33:42 +0530

    [Minor Fix]:  -Minor fix in price attribute of food items

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -9,7 +9,7 @@ class FoodItem < ApplicationRecord
   scope :filter_by_creation, -> { order(created_at: :desc) }
 
   validates :name, presence: true, length: { maximum: 50 }
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 1 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :description, presence: true, length: { maximum: 255 }
   validates :user_id, presence: true
 
